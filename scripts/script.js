49c47d7fcc3f188f72e2e88905ddc13d889a297e
James Jackson <james.ogge@gmail.com>
2017-03-07 17:25:25 +0000

    setup

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+var images = {
+  images: [
+    {
+      id: 0,
+      src: "images/lena.bmp"
+    },
+    {
+      id: 1,
+      src: "images/lena.EX0423.jpg"
+    }
+  ]
+};
+
+function showChoice(){
+  
+}
