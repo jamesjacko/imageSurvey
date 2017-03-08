544fcd3420e79d4174314c6a7b54aefd34310712
James Jackson <james.ogge@gmail.com>
2017-03-08 16:53:14 +0000

    staring

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,40 @@
-var images = {
-  images: [
-    {
-      id: 0,
-      src: "images/lena.bmp"
-    },
-    {
-      id: 1,
-      src: "images/lena.EX0423.jpg"
-    }
-  ]
-};
+function showImage(options){
 
-function showChoice(){
-  
+}
+
+
+
+function init(_settings){
+  document.body.innerHTML += '<div id="choices"></div>';
+  showChoices(_settings);
+}
+
+function showChoices(_settings){
+  var choices = randperm(_settings.items.length);
+  for (var i = 0; i < _settings.numberPerQuestion; i++) {
+    if(typeof _settings.itemFunction === "function"){
+      _settings.itemFunction(_settings.items[i].options);
+      console.log("here12");
+    }
+    else
+      console.error("You must provide an item as a function");
+  }
+}
+
+
+// return a random permutation of a range (similar to randperm in Matlab)
+function randperm(length){
+    // first generate number sequence
+    var permArray = new Array(length);
+    for(var i = 0; i < length; i++){
+        permArray[i] = i;
+    }
+    // draw out of the number sequence
+    for (var i = (length - 1); i >= 0; --i){
+        var randPos = Math.floor(i * Math.random());
+        var tmpStore = permArray[i];
+        permArray[i] = permArray[randPos];
+        permArray[randPos] = tmpStore;
+    }
+    return permArray;
 }
